9aed52201b3bbdabc369f77f32cccea5b4fa4017
Brock Ellis <brock@sharproot.com>
2017-05-22 09:12:04 -0500

    Made index page to host landing page for all projects

---
+++ assignments/youtube-embed/README.md
@@ -0,0 +1,2 @@
+# Youtube-Embed
+Learning containers with auto width and embedding youtube videos.
---
+++ scripts.js
@@ -0,0 +1,25 @@
+var flakes = [];
+
+function animationLoop(){
+	//step 1
+	checkFlakes();
+
+	//step 2
+	resetFlakes();
+
+	//step 3
+	moveFlakes();
+
+}
+
+// Step 1
+function checkFlakes(){
+	
+	//Have to loop backwards to stop indexes from getting screwed up
+	for(var i = flakes.lenght - 1; i >= 0; i--){
+
+
+	}
+
+
+}
